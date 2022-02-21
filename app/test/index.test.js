689b7a3b53e7eb3ef3f61dd52362242af04dcc53
Eliazar Lopez <44718686+eliazarnd@users.noreply.github.com>
2022-02-20 16:24:05 -0800

    add test to count number of consonants

--- app/test/index.test.js
+++ app/test/index.test.js
@@ -3,7 +3,9 @@ const assert = chai.assert;
 const should = chai.should();
 const excpect = chai.expect();
 
-const { isVowel, countVowels } = require('../utils/utils');
+const { isVowel, 
+        countVowels, 
+        countConsonants } = require('../utils/utils');
 
 describe('Helpers Tests', function () {
   describe('#isVowel', function () {
@@ -18,20 +20,30 @@ describe('Helpers Tests', function () {
     });
 
     it('should return if character is not a vowel', function () {
-        
+        //Arrange
         const input = "x";
+        //Act
         const result = isVowel(input);
-
+        //Asssert
         result.should.be.equal(false);
     });
 
     it('should return number of vowels that contains a word', function () {
-        
+        //Arrange
         const input = "testing word";
+        //Act
         const result = countVowels(input);
-
+        //Assert
         result.should.be.equal(3);
     });
 
+    it('should return number of consonants that contains a word', function () {
+        //Arrange
+        const input = "testing word";
+        //Act
+        const result = countConsonants(input);
+        //Assert
+        result.should.be.equal(8);
+    });
   });
 });
