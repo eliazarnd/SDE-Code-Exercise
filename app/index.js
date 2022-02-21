0647f9e77808b85da5c06612a0c64d180f1dad39
Eliazar Lopez <44718686+eliazarnd@users.noreply.github.com>
2022-02-20 16:58:18 -0800

    implement basic funcition to read and file turn into array

--- app/index.js
+++ app/index.js
@@ -1,1 +1,54 @@
-console.log("Init project");
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs')
+//Setting
+const mockupsRefs = "/mockups"
+const directoryPath = path.join(__dirname, mockupsRefs);
+
+function readFile(fileName = ""){
+
+    try {
+        let contain = fs.readFileSync(`${directoryPath}/${fileName}.txt`).toString();
+
+        return contain;
+
+    } catch (error) {
+        throw new Error("file does not exist");
+    }
+
+   
+}
+
+function getDrivers(){
+    try {
+        let drivers = readFile("drivers");
+
+        return drivers.trim().split("\n");
+        
+    } catch (error) {
+        console.error(error)        
+    }
+   
+}
+
+function getShipmentsDestinations(){
+
+    try {
+        let shipmentDestination = readFile("shipmentsDestination");
+        return shipmentDestination.trim().split("\n");
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+const driversArray = getDrivers();
+const shipmentsDestinationArray = getShipmentsDestinations();
+
+
+/**(async () => {
+    await init();
+  
+    //await login(credentials);
+  })()
+ */
